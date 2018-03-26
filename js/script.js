7b873c9c43e4a19b651db0c1c9cb54da54456831
Junior Rausch <junior.rausch@gmx.de>
2018-03-26 18:14:15 +0200

    template choose button

--- js/script.js
+++ js/script.js
@@ -1,41 +1,48 @@
-$('.ui.dropdown')
-	.dropdown();
+$(function(){
+	$('.ui.dropdown')
+		.dropdown();
 
-$('.ui.accordion')
-	.accordion();
-
-
-$('.ui.rating')
-	.rating();
-// Modal Function 
-$(".border-btn").click(function () {
-	$('.ui.modal').modal('show');
-});
-
-// delete Account
-$(".del-account").click(function () {
-	$('.del-modal').modal({
-			inverted: true
-		})
-		.modal('show');
-});
+	$('.ui.accordion')
+		.accordion();
 
 
-// Open Overlay add payment
-$(".bill-con.addy").click(function () {
-	$('.ui.modal').modal('show');
-});
+	$('.ui.rating')
+		.rating();
+	// Modal Function 
+	$(".border-btn").click(function () {
+		$('.ui.modal').modal('show');
+	});
 
-// add new section CV editor
-$(".add-sect.new").click(function () {
-	$('.ui.new_section.modal').modal('show');
-});
+	// delete Account
+	$(".del-account").click(function () {
+		$('.del-modal').modal({
+				inverted: true
+			})
+			.modal('show');
+	});
 
-//tab menu
-$('.menu .item')
-	.tab();
 
-// CV Modal
-$(".modal-cv").click(function () {
-	$('.ui.open-cv.modal').modal('show');
-});
+	// Open Overlay add payment
+	$(".bill-con.addy").click(function () {
+		$('.ui.modal').modal('show');
+	});
+
+	// add new section CV editor
+	$(".add-sect.new").click(function () {
+		$('.ui.new_section.modal').modal('show');
+	});
+
+	//tab menu
+	$('.menu .item')
+		.tab();
+
+	// CV Modal
+	$(".modal-cv").click(function () {
+		$('.ui.open-cv.modal').modal('show');
+	});	
+	// Closing modal 
+	$('.close-modal').click(function(){
+		$('.ui.modal').hide();
+	});
+	
+}); // JQuery
